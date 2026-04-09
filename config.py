93fefb681c2515ec4bc0bdad2d1e4ef625ef2c97
Idung Emmanuel <idungemmanuel7@gmail.com>
2026-04-09 12:02:11 +0100

    Create config.py file with API keys and trading parameters

---
+++ config.py
@@ -0,0 +1,14 @@
+# Configuration settings for Bybit and MetaTrader5 APIs
+
+BYBIT_API_KEY = 'your_bybit_api_key'
+BYBIT_API_SECRET = 'your_bybit_api_secret'
+
+MT5_LOGIN = 'your_mt5_login'
+MT5_PASSWORD = 'your_mt5_password'
+
+# Trading Parameters
+TRADING_SYMBOL = 'BTCUSD'
+TRADE_AMOUNT = 0.01
+ORDER_TYPE = 'LIMIT'
+TAKE_PROFIT = 50.0
+STOP_LOSS = 50.0
